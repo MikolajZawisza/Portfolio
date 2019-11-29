1b0eef0603b2e76d631b1ccf5794eaa170a49309
MikolajZawisza <mikolaj.zawisza.96@gmail.com>
2019-11-29 22:19:46 +0100

    Second Activity fixed

--- app/src/main/java/com/example/mikol/portfolio/MainActivity.java
+++ app/src/main/java/com/example/mikol/portfolio/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.mikol.portfolio;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -9,6 +10,7 @@ import android.widget.ListView;
 import android.widget.Toast;
 import java.util.Arrays;
 import java.util.List;
+
 
 import static com.example.mikol.portfolio.ProjectTable.DB_NAME;
 import static com.example.mikol.portfolio.ProjectTable.DB_VERSION;
@@ -29,26 +31,39 @@ public class MainActivity extends AppCompatActivity {
         ListView listView = findViewById(R.id.projectList);
         List<Project> projects = Arrays.asList
                 (
-                new Project(0, "Portfolio", "Ala ma kota kot ma ale", "patt yo code","path to photo"),
-                new Project(0,"Kotek LUAN", "Luan ma Ale,Ala ma Luan","patt yo code","path to photo")
+            //    new Project(0, "Portfolio", "Ala ma kota kot ma ale", "patt yo code","path to photo"),
+             //   new Project(0,"Kotek LUAN", "Luan ma Ale,Ala ma Luan","patt yo code","path to photo")
                  );
         for(int i = 0; i < projects.size(); i++){
             projectDao.save(projects.get(i));
         }
+
         List<Project> studentsToShow = projectDao.getAll();
         adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, studentsToShow);
         listView.setAdapter(adapter);
+
         listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+
             @Override
             public boolean onItemLongClick(AdapterView<?> adapterView, View view, int i, long l) {
                 Project project = (Project) adapterView.getItemAtPosition(i);
-                Toast toast = Toast.makeText(getApplicationContext(),
+
+                    goToAnActivity(view);
+
+                /*Toast toast = Toast.makeText(getApplicationContext(),
                         project.getName(),
                         Toast.LENGTH_SHORT);
-                toast.show();
+                toast.show();*/
                 return false;
             }
         });
 
+
     }
+
+    public void goToAnActivity(View view) {
+        Intent intent = new Intent(this, Main2Activity.class);
+        startActivity(intent);
+    }
+
 }
